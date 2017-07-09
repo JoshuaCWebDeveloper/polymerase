f0dd34ac19f3f8e9e92119c47473fd293dc586d7
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2017-07-08 18:43:34 -0700

    Create polymerase.js that exports empty main function

---
+++ build/polymerase.js
@@ -0,0 +1,14 @@
+/* polymerase.js
+ * Main project file, defines and exports main function
+ * Dependencies: 
+ * Author: Joshua Carter
+ * Created: July 08, 2017
+ */
+"use strict";
+
+//define main function
+
+function main() {}
+
+//export main function
+module.exports = main;
